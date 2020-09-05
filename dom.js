f47c0382dc5e16690690e8b4ba5bf1d892b42886
klutest200 <klu9915005200@gmail.com>
2020-09-05 09:30:25 +0530

    Query Selector Examples

--- dom.js
+++ dom.js
@@ -1,4 +1,26 @@
 //DOM is object oriented representation of the HTML
 
 // To change the title;
-document.title = "DOM Intro";
+
+const headers = document.getElementsByTagName('h2');
+console.log(headers); // Container looking like an array;
+
+// By class name
+const list = document.getElementsByClassName('item');
+console.log(list);
+
+//By ID
+const button = document.getElementById('submit');
+console.log(button);
+
+//Query Selector
+const headers1 = document.querySelector('h2');
+console.log(headers1);
+const nodeList = document.querySelectorAll('.item');
+console.log(nodeList);
+
+const listAnchor = document.querySelector('#list');
+console.log(listAnchor);
+
+const listAnchors = document.querySelectorAll('#list a');
+console.log(listAnchors);
